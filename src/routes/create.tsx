87626264473766cc43ab7164cff2435a9275eec8
Deepanshu tyagi <deepanshu123tyagi@gmail.com>
2022-05-10 14:41:43 +0530

    create page

--- src/routes/create.tsx
+++ src/routes/create.tsx
@@ -9,7 +9,15 @@ import { Link } from "react-router-dom"
 import { checkWalletIsConnected, connectWalletHandler } from "../components/LoadBlockchain"
 
 
-
+interface Data {
+    itemName: string,
+    description:string,
+    url:string,
+    TokenStandard:string,
+    BlockChain:string,
+    tokenCreator:string,
+    forSale:boolean
+}
 
 
 
@@ -21,6 +29,7 @@ const Create = () => {
     let [jsonCid, setJsonCid] = useState("");
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    
 
 
     useEffect(() => {
@@ -36,70 +45,71 @@ const Create = () => {
     }, [currentAccount])
 
 
-    // const fileHandler = async (e) => {
-    //     const file = e.target.files[0];
-    //     let data = new FormData();
-    //     data.append('file', file)
-    //     try {
-    //         axios.post(url, data, {
-    //             headers: {
-    //                 maxBodyLength: 'Infinity',
-    //                 pinata_api_key: pinataApiKey,
-    //                 pinata_secret_api_key: pinataSecretApiKey
-    //             }
-    //         }).then((response) => {
-    //             console.log("image uploaded")
-    //             let imageUrl = `https://gateway.pinata.cloud/ipfs/${response.data.IpfsHash}`
-    //             setFileUrl(imageUrl)
-    //         })
-    //     } catch (error) {
-    //         console.log('Error uploading file: ', error)
-    //     }
-    // }
+    const fileHandler = async (e) => {
+        const file = e.target.files[0];
+        let data = new FormData();
+        data.append('file', file)
+        try {
+            axios.post(url, data, {
+                headers: {
+                    maxBodyLength: 'Infinity',
+                    pinata_api_key: pinataApiKey,
+                    pinata_secret_api_key: pinataSecretApiKey
+                }
+            }).then((response) => {
+                console.log("image uploaded")
+                let imageUrl = `https://gateway.pinata.cloud/ipfs/${response.data.IpfsHash}`
+                setFileUrl(imageUrl)
+            })
+        } catch (error) {
+            console.log('Error uploading file: ', error)
+        }
+    }
 
 
-    // const mintToken = async (hash, base) => {
-    //     let txn = await mintNftHandler(hash, base);
-    //     return txn;
-    // }
+    const mintToken = async (hash, base) => {
+        let txn = await mintNftHandler(hash, base);
+        return txn;
+    }
 
 
-    // const jsonHandler = (data) => {
-    //     const jsonData = JSON.stringify(data);
-    //     try {
-    //         axios.post(jsonUrl, jsonData, {
-    //             headers: {
-    //                 pinata_api_key: pinataApiKey,
-    //                 pinata_secret_api_key: pinataSecretApiKey
-    //             }
-    //         }).then(async (response) => {
-    //             setJsonCid(response.data.IpfsHash)
-    //             let txn = await mintToken(response.data.IpfsHash, "https://gateway.pinata.cloud/ipfs/");
-    //             data["tokenCreator"] = txn.from.slice(2,);
-    //             data["currentOwner"] = txn.from.slice(2,);
-    //             data["previousOwner"] = "0000000000000000000000000000000000000000";
-    //             axios.post('http://localhost:5000/mintToken', data).then((response) => {
-    //             })
-    //             setTokenMinted(true);
-    //         })
-    //     } catch (error) {
-    //         console.log('Error uploading file: ', error)
-    //     }
-    // }
+    const jsonHandler = (data) => {
+        const jsonData = JSON.stringify(data);
+        try {
+            axios.post(jsonUrl, jsonData, {
+                headers: {
+                    pinata_api_key: pinataApiKey,
+                    pinata_secret_api_key: pinataSecretApiKey
+                }
+            }).then(async (response) => {
+                setJsonCid(response.data.IpfsHash)
+                let txn = await mintToken(response.data.IpfsHash, "https://gateway.pinata.cloud/ipfs/");
+                data["tokenCreator"] = txn.from.slice(2,);
+                data["currentOwner"] = txn.from.slice(2,);
+                data["previousOwner"] = "0000000000000000000000000000000000000000";
+                axios.post('http://localhost:5000/mintToken', data).then((response) => {
+                })
+                setTokenMinted(true);
+            })
+        } catch (error) {
+            console.log('Error uploading file: ', error)
+        }
+    }
 
-    // const uploadHandler = async (e) => {
-    //     e.preventDefault();
-    //     let data = {};
-    //     data['itemName'] = e.target.item.value.trim();
-    //     data["description"] = e.target.description.value;
-    //     data["url"] = fileUrl;
-    //     data["TokenStandard"] = "ERC-721";
-    //     data["BlockChain"] = "Ethereum";
-    //     data["tokenCreator"] = currentAccount;
-    //     data["forSale"] = false;
-    //     // data[transactionHistory] = [];
-    //     await jsonHandler(data);
-    // }
+    const uploadHandler = async (e :React.FormEvent<EventTarget>) => {
+        e.preventDefault();
+        let data: Data = {
+            itemName:e.target.item.value.trim(),
+            description:e.target.description.value,
+            url:fileUrl,
+            TokenStandard:"ERC-721",
+            BlockChain:"Ethereum",
+            tokenCreator:currentAccount,
+            forSale:false,
+    };
+       
+        await jsonHandler(data);
+    }
     const connectWalletButton = () => {
         const connectWallet = async () => {
             let account = await connectWalletHandler();
@@ -113,37 +123,38 @@ const Create = () => {
             </div>
         )
     }
-    // const createNft = () => {
-    //     return (
-    //         <div >
-    //             <h1>Create new Item</h1>
-    //             <Form className="create-page-form" onSubmit={uploadHandler}>
-    //                 <Form.Group className="mb-3" >
-    //                     <Form.Label>Image, Video, Audio, or 3D Model<span style={{ color: 'red' }} >*</span></Form.Label>
-    //                     <Form.Control type="file" placeholder="File" onChange={fileHandler} />
-    //                 </Form.Group>
-    //                 <Form.Text className="text-muted">
-    //                     <span style={{ color: 'red' }} >*</span>Required fields
-    //                 </Form.Text>
-    //                 <Form.Group className="mb-3" >
-    //                     <Form.Label>Name<span style={{ color: 'red' }} >*</span></Form.Label>
-    //                     <Form.Control type="text" name="item" placeholder="Item name" required />
-    //                 </Form.Group>
-    //                 <Form.Group className="mb-3" >
-    //                     <Form.Label>Description</Form.Label>
-    //                     <Form.Control style={{ padding: '10px 10px 50px 10px' }} type="text" name="description" placeholder="Provide a detailed description of your item" />
-    //                     <Form.Text className="text-muted">
-    //                         The description will be included on the item's detail page underneath its image.
-    //                     </Form.Text>
-    //                 </Form.Group>
-    //                 <hr />
-    //                 <Button variant="primary" type="submit" onClick={handleShow}>
-    //                     Create
-    //                 </Button>
-    //             </Form>
-    //         </div>
-    //     )
-    // }
+    const createNft = () => {
+        return (
+            <div >
+                <h1>Create new Item</h1>
+                <Form className="create-page-form" onSubmit={uploadHandler}>
+                    <Form.Group className="mb-3" >
+                        <Form.Label>Image, Video, Audio, or 3D Model<span style={{ color: 'red' }} >*</span></Form.Label>
+                        <Form.Control type="file" placeholder="File" onChange={fileHandler} />
+                    </Form.Group>
+                    <Form.Text className="text-muted">
+                        <span style={{ color: 'red' }} >*</span>Required fields
+                    </Form.Text>
+                    <Form.Group className="mb-3" >
+                        <Form.Label>Name<span style={{ color: 'red' }} >*</span></Form.Label>
+                        <Form.Control type="text" name="item" placeholder="Item name" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" >
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control style={{ padding: '10px 10px 50px 10px' }} type="text" name="description" placeholder="Provide a detailed description of your item" />
+                        <Form.Text className="text-muted">
+                            The description will be included on the item's detail page underneath its image.
+                        </Form.Text>
+                    </Form.Group>
+                    <hr />
+                    <Button variant="primary" type="submit" onClick={handleShow}>
+                        Create
+                    </Button>
+                </Form>
+                
+            </div>
+        )
+    }
     const accountChanged = async () => {
         const { ethereum } = window;
 
@@ -177,7 +188,7 @@ const Create = () => {
                 </Modal.Body>
             </Modal>
             <div className="create-page-document">
-                { <h3>Connect your wallet first</h3>}
+                { currentAccount ? createNft() : <h3>Connect your wallet first</h3>}
             </div>
         </div>
     )
